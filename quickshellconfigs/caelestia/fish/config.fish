0565420efc83eaec9842722450ce65758f9c51b2
SimplyRohit <rohitjaatjaat073@gmail.com>
2025-07-13 15:50:20 +0530

    new conf

---
+++ quickshellconfigs/caelestia/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Starship custom prompt
+    starship init fish | source
+
+    # Custom colours
+    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+
+    # For jumping between prompts in foot terminal
+    function mark_prompt_start --on-event fish_prompt
+        echo -en "\e]133;A\e\\"
+    end
+end
+export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
